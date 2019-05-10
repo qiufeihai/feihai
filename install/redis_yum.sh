c2f41cbc25466cd48c2e70f220bd283c1591c041
qiufeihai <308350718@qq.com>
2019-05-11 01:34:41 +0800

    move file

---
+++ install/redis_yum.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# install redis with yum 
+
+log() {
+  echo -e  "\e[1;35m------------------------ $@ ------------------------------\e[0m"
+}
+
+log 安装并启用remi源
+sudo yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum-config-manager --enable remi
+
+log 安装redis
+sudo yum install redis
+
+log systemctl 启动和开机启动
+sudo systemctl start redis
+sudo systemctl enable redis
