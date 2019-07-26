09d0816e47d6c05b3d5202dd6976ddbe6aeca12b
root <root@DESKTOP-G3C2S9M.localdomain>
2019-07-26 14:19:11 +0800

    更新redis版本

--- install/redis.sh
+++ install/redis.sh
@@ -6,14 +6,14 @@ log() {
 }
 
 log 下载
-wget http://download.redis.io/releases/redis-4.0.11.tar.gz
-tar xzf redis-4.0.11.tar.gz
-cd redis-4.0.11
+wget http://download.redis.io/releases/redis-5.0.5.tar.gz
+tar xzf redis-5.0.5.tar.gz
+cd redis-5.0.5
 log 编译
 make
 log 软连接
 [ ! -d $HOME/bin ] &&  mkdir $HOME/bin
-ln -s $PWD/redis-4.0.11/src/redis-server $HOME/bin/redis-server
-ln -s $PWD/redis-4.0.11/src/redis-cli $HOME/bin/redis-cli
+ln -s $PWD/redis-5.0.5/src/redis-server $HOME/bin/redis-server
+ln -s $PWD/redis-5.0.5/src/redis-cli $HOME/bin/redis-cli
 
 
