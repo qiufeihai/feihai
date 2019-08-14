dbe61933802d6e56136db68afd2455ccd7d2dce7
root <root@DESKTOP-G3C2S9M.localdomain>
2019-08-14 12:02:56 +0800

    add nginx_add_module.sh

---
+++ script/nginx_add_module.md
@@ -0,0 +1,3 @@
+```
+curl -o- https://raw.githubusercontent.com/qiufeihai/feihai/master/script/nginx_add_module.sh > nginx_add_module.sh && chmod +x nginx_add_module.sh
+```
---
+++ script/nginx_add_module.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# author: feihai
+# nginx add module script
+log() {
+  echo -e  "\e[1;35m------------------------ $@ ------------------------------\e[0m"
+}
+[[ "$1" == '-h' || "$1" == '--help' || "$1" == "" ]] && {
+  echo "Usage: $0 <configue options> [nginx_version]"
+  exit 0
+}
+
+if ! type -p nginx &>/dev/null; then
+  log 请先安装nginx
+  exit 0
+fi
+
+dir=/tmp/nginx
+mkdir -p $dir
+cd $dir
+nginx_version=`nginx -v 2>&1 | grep -oP '/\K.*'`
+nginx_version=${nginx_version:-1.16.0}
+[ $2 ] && nginx_version=$2
+log nginx_version: $nginx_version
+[ -f nginx-${nginx_version}.tar.gz ] || {
+wget http://nginx.org/download/nginx-${nginx_version}.tar.gz
+tar -zxvf nginx-${nginx_version}.tar.gz -C .
+}
+cd nginx-${nginx_version}
+cp `which nginx`{,.bak}
+log 备份旧nginx命令为 `which nginx`.bak
+#echo " `nginx -V 2>&1 | sed "s/\s\+--/ --/g" | grep -oP 'configure arguments:\K.*'` $1" | xargs -I {} ./configure {}
+echo " `nginx -V 2>&1 | sed "s/\s\+--/ --/g" | grep -oP 'configure arguments:\K.*'` $1" > $dir/args
+log 复制以下语句执行
+echo "cd $dir/nginx-${nginx_version}/ && ./configure  `cat $dir/args` && make && make install && cp $dir/nginx-${nginx_version}/objs/nginx `which nginx`"
+#log 覆盖nginx命令，复制并执行：cp $dir/nginx-${nginx_version}/objs/nginx `which nginx`
