a3428cfd10ac9af542b677a0f040018b37351b4a
qiufeihai <308350718@qq.com>
2020-03-01 23:30:06 +0800

    nginx换成openresty

--- install/openresty.sh
+++ install/openresty.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # author: feihai
 # install openresty
-
+# 目录对比nginx -->  openresty
+# home: /etc/nginx  -->  /usr/local/openresty/nginx
+# html: /usr/share/nginx/html  -->  /usr/local/openresty/nginx/html
 log() {
   echo -e  "\e[1;35m------------------------ $@ ------------------------------\e[0m"
 }
@@ -16,7 +18,7 @@ log 配置PATH
 [ -e ~/.zshrc ] && echo 'export PATH=/usr/local/openresty/nginx/sbin:$PATH' >> ~/.zshrc
 [ -e ~/.bashrc ] && echo 'export PATH=/usr/local/openresty/nginx/sbin:$PATH' >> ~/.bashrc
 
-mkdir -p /usr/local/openresty/nginx/conf.d
+mkdir -p /usr/local/openresty/nginx/conf/conf.d
 
 log 启动服务，开机自启动
 systemctl start openresty
