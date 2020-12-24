9e280c36be6c93f5d3c5a9293ab7a1d5e870ad35
qfh <qfh@xxx.com>
2020-12-24 18:43:59 +0800

    安装nvm改成git clone方式

--- install/node.sh
+++ install/node.sh
@@ -6,9 +6,16 @@ log() {
 }
 
 log 安装nvm
-git clone https://github.com/cnpm/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
-echo ". ~/.nvm/nvm.sh" >> /etc/profile
-source /etc/profile
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+log 引入nvm到zshrc
+echo  'export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.zshrc
+source $HOME/.zshrc
 
 log nvm加速
 export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node
